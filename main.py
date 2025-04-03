7dbac07b19aeb09abfd19236b1934a237a0f3e3a
yakovmahgerefteh <mahgereftehyakov18@gmail.com>
2025-04-03 17:06:11 -0400

    add in first file

---
+++ main.py
@@ -0,0 +1,26 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+def in_autotests_we_trust(a, b):
+    if a == b:
+        print('Test passed')
+    else:
+        print('Test failed')
+
+in_autotests_we_trust(10, '10')
+
+in_autotests_we_trust(0, False)
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
